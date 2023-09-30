a82fc41c4a422db25fc7496a03646426fcea4283
Jack Christensen <jack@jackchristensen.com>
2023-09-30 10:02:47 -0500

    Add copied Go implementation for benchmarking
    Leaving existing top-level one around to not break the Android / Java
    one.

---
+++ go/cgol/cmd/bench/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/jackc/cgol/go/cgol"
+)
+
+var options struct {
+	width     int
+	height    int
+	cellCount int
+	stepCount int
+	seed      int
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage:  %s [options]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.IntVar(&options.width, "width", 100, "width of world")
+	flag.IntVar(&options.height, "height", 100, "height of world")
+	flag.IntVar(&options.cellCount, "livecount", 100, "live cells to start")
+	flag.IntVar(&options.stepCount, "stepcount", 10_000, "number of steps to run")
+	flag.IntVar(&options.seed, "seed", -1, "seed")
+	flag.Parse()
+
+	w := cgol.NewWorld(options.width, options.height)
+
+	rng := rand.New(rand.NewSource(int64(options.seed)))
+
+	for i := 0; i < options.cellCount; i++ {
+		w.Set(rng.Intn(w.Width()), rng.Intn(w.Height()), true)
+	}
+
+	startTime := time.Now()
+	for i := 0; i < options.stepCount; i++ {
+		w = w.Step()
+	}
+	endTime := time.Now()
+
+	duration := endTime.Sub(startTime)
+	fmt.Println(duration.Seconds())
+}
---
+++ go/cgol/go.mod
@@ -0,0 +1,7 @@
+module github.com/jackc/cgol/go/cgol
+
+go 1.21.1
+
+require github.com/nsf/termbox-go v1.1.1
+
+require github.com/mattn/go-runewidth v0.0.9 // indirect
